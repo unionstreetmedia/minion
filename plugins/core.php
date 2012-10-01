35448a8d4f74a9668c6c61d17492895209e84558
Ryan Freebern <ryan@freebern.org>
2012-10-01 12:17:03 -0400

    Changes to allow access to configuration; added nick collision handling.

--- plugins/core.php
+++ plugins/core.php
@@ -1,22 +1,29 @@
 <?php
 
-return new Plugin(
+$Core = new Plugin(
     'Core',
     'Minion core functionality.',
     'Ryan N. Freebern / ryan@freebern.org',
+    array()
+);
 
-    array(
-        'connect' => function (&$minion, &$data) {
-            if ($this->conf('Password')) {
-                $minion->send("PASS {$this->conf('Password')}");
+$Core->On = array(
+        'connect' => function (&$minion, &$data) use ($Core) {
+            if ($Core->conf('Password')) {
+                $minion->send("PASS {$Core->conf('Password')}");
             }
-            $minion->send("USER {$this->conf('Nick')} hostname {$this->conf('RealName')}");
-            $minion->Send("NICK {$this->conf('Nick')}");
+            $minion->send("USER {$Core->conf('Nick')} hostname {$Core->conf('RealName')}");
+            $minion->Send("NICK {$Core->conf('Nick')}");
         },
         'PING' => function (&$minion, &$data) {
-            $minion->send("PONG {$data['target']}");
+            $minion->send("PONG {$data['message']}");
+        },
+        '433' => function (&$minion, &$data) {
+            $nick = $Core->conf('Nick') . (string) rand(1,999);
+            $minion->send("NICK {$nick}");
         }
-    )
-);
+    );
+
+return $Core;
 
 ?>
